f3912839d34a7664f13f0143802ef4d7eb574159
lalbanese <libby@albanese.com>
2023-11-29 23:42:44 -0500

    Updated build

---
+++ precache-manifest.08b2bf6b12eccf7ad208dc84dc0e7c7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e29a1d214f3ef93d6dc965cfe6472f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3606f996ecfe55825da0",
+    "url": "/static/js/2.a7481279.chunk.js"
+  },
+  {
+    "revision": "b1cf8857018de83f14f35e442377e2b9",
+    "url": "/static/js/2.a7481279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd4b9e73055e18ee722",
+    "url": "/static/js/main.0d2ddf00.chunk.js"
+  },
+  {
+    "revision": "d374c2088abf142abe6c",
+    "url": "/static/js/runtime-main.9be14982.js"
+  }
+]);
